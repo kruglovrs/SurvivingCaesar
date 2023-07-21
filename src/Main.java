919a928520a231643a67131ee2bfb3e7eed8a497
Kruglov Roman <kruglovrs@gmail.com>
2023-07-21 12:55:00 +0300

    added ukrainian alphabet ArrayList

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,24 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.printf("Hello and welcome!");
+        ArrayList<Character> alphabet = new ArrayList<>();
+        for (char letter = 'А'; letter <= 'Я'; letter++) {
+            alphabet.add(letter);
+        }
+        alphabet.add('Є');
+        alphabet.add('Ї');
+        alphabet.add('.');
+        alphabet.add(',');
+        alphabet.add('”');
+        alphabet.add(':');
+        alphabet.add('-');
+        alphabet.add('!');
+        alphabet.add('?');
+        alphabet.add(' ');
+
+
+        System.out.println(alphabet);
     }
 }
