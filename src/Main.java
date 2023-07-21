49ca7ed861867248d6e29a65302949a6f5424abb
Kruglov Roman <kruglovrs@gmail.com>
2023-07-21 14:36:01 +0300

    add method encrypt, read file from string-paht, write new same file by static path yet

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import java.io.*;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
@@ -8,6 +10,7 @@ public class Main {
             alphabet.add(letter);
         }
         alphabet.add('Є');
+        alphabet.add('Ґ');
         alphabet.add('Ї');
         alphabet.add('.');
         alphabet.add(',');
@@ -18,7 +21,31 @@ public class Main {
         alphabet.add('?');
         alphabet.add(' ');
 
-
         System.out.println(alphabet);
+        System.out.println(alphabet.size());
+        System.out.println("Введіть шлях до файлу, який треба закодувати");
+        Scanner scanner = new Scanner(System.in);
+        String src = scanner.nextLine();
+        encrypt(src,0);
+
+    }
+
+    private static void encrypt(String src, int key) {
+        String dest = "C:\\Users\\Acer\\OneDrive\\Рабочий стол\\Zomro_new.txt";
+        try(FileReader reader = new FileReader(src);
+            FileWriter writer = new FileWriter(dest))
+        {
+            char[] buffer = new char[65536]; // 128Kb
+            while (reader.ready())
+            {
+                int real = reader.read(buffer);
+                writer.write(buffer, 0, real);
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println(dest);
     }
 }
