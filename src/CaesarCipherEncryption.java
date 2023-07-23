4bf2e4649d5ed54f032b37f63a3c461d63259e80
Kruglov Roman <kruglovrs@gmail.com>
2023-07-23 12:09:32 +0300

    implemented a method to decrypt the file

--- src/CaesarCipherEncryption.java
+++ src/CaesarCipherEncryption.java
@@ -19,6 +19,22 @@ public class CaesarCipherEncryption {
 
     }
 
+    private static char decryptChar(char ch, int shift, ArrayList<Character> alphabet) {
+        if (Character.isLetter(ch)) {
+            int alphabetSize = alphabet.size();
+            int currentIndex = alphabet.indexOf(Character.toUpperCase(ch));
+            //int newIndex = (currentIndex + shift) % alphabetSize;
+            int newIndex = (currentIndex - shift) % alphabetSize;
+            if (Character.isLowerCase(ch)) {
+                return Character.toLowerCase(alphabet.get(newIndex));
+            } else {
+                return alphabet.get(newIndex);
+            }
+        }
+        return ch;
+
+    }
+
     public static void encryptFile(String inputFilePath, String outputFilePath, int shift, ArrayList<Character> ukrainianAlphabet) {
         try (BufferedReader reader = new BufferedReader(new FileReader(inputFilePath));
              BufferedWriter writer = new BufferedWriter(new FileWriter(outputFilePath))) {
@@ -36,6 +52,22 @@ public class CaesarCipherEncryption {
         }
     }
 
+    public static void decryptFile(String inputFilePath, String outputFilePath, int shift, ArrayList<Character> ukrainianAlphabet) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(inputFilePath));
+             BufferedWriter writer = new BufferedWriter(new FileWriter(outputFilePath))) {
+
+            int ch;
+            while ((ch = reader.read()) != -1) {
+                char decryptedChar = decryptChar((char) ch, shift, ukrainianAlphabet);
+                writer.write(decryptedChar);
+            }
+
+            System.out.println("Файл успішно разшифрований і збережений у " + outputFilePath);
+
+        } catch (IOException e) {
+            System.err.println("Помилка під час дешифрування файлу: " + e.getMessage());
+        }
+    }
     public static void main(String[] args) {
 
         ArrayList<Character> ukrainianAlphabet = new ArrayList<>();
@@ -68,7 +100,14 @@ public class CaesarCipherEncryption {
 
             CaesarCipherEncryption.encryptFile(inputFilePath, outputFilePath, shift, ukrainianAlphabet);
         } else {
-            System.out.println("Поки ще не готово");
+            System.out.print("Введіть шлях до вихідного текстового файлу: ");
+            String inputFilePath = scanner.nextLine();
+            System.out.print("Введіть шлях для збереження дешифрованого файлу: ");
+            String outputFilePath = scanner.nextLine();
+            System.out.print("Введіть ключ для шифру Цезаря (ціле число): ");
+            int shift = scanner.nextInt();
+
+            decryptFile(inputFilePath, outputFilePath, shift, ukrainianAlphabet);
         }
     }
 }
